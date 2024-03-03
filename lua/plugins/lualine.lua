1572b73af0469dd14ea2a1adb90751713e3d0dc1
Odair M. <odairmario45@gmail.com>
2024-03-03 19:08:24 -0300

    feat(Plugin): add lualine power line theme

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,12 @@
+-- the powerline theme
+return {
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    require('lualine').setup({
+      options = {
+        theme = 'dracula'
+      }
+    })
+  end
+}
+
