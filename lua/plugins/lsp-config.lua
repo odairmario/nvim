52f4e7acf69cbd2c28eb95641a79e92e9b8e3799
Odair M. <odairmario45@gmail.com>
2024-03-03 19:09:55 -0300

    feat(Plugin): add lsp mason and nvim-lsp plguins

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "tsserver", "jedi_language_server", "pyre", "pyright", "pylsp", "ruff_lsp"}
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      local lspconfig = require("lspconfig")
+
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities
+      })
+      lspconfig.jedi_language_server.setup({
+        capabilities = capabilities
+      })
+      lspconfig.pyright.setup({
+        capabilities = capabilities
+      })
+      lspconfig.pylsp.setup({
+        capabilities = capabilities
+      })
+      lspconfig.ruff_lsp.setup({
+        capabilities = capabilities
+      })
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition,{})
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references,{})
+      vim.keymap.set({'n','v'}, '<leader>ca', vim.lsp.buf.code_action,{})
+    end
+  }
+}
