29b58ddd971dd5b50cf16fa78ffcaf839331f270
Odair M. <odairmario45@gmail.com>
2024-03-04 08:04:40 -0300

    fix(Plugin): remove erb_d from the list of formattings plugins

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,6 +7,7 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
+        null_ls.builtins.diagnostics.erb_lint,
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
 				null_ls.builtins.diagnostics.rubocop,
