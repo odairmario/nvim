5109f5ca68164927a14cc31b13ff833240667823
Odair M. <odairmario45@gmail.com>
2024-03-04 08:03:49 -0300

    fix(Plugin): fix completions mappings

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -26,11 +26,11 @@ return {
           documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
-          ["<C-j>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-k>"] = cmp.mapping.scroll_docs(4),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
