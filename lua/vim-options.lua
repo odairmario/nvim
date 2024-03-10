5b2b46cabf2fa45159d4cf0fac07b3a65ad22267
Odair M. <odairmario45@gmail.com>
2024-03-10 17:45:58 -0300

    fix(General): add more vim options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,6 +6,14 @@ vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.cmd("set number")
 vim.cmd("set textwidth=100")
+vim.cmd("set nowrap")
+
+vim.api.nvim_create_autocmd("FileType",  {
+  command = [[ :set tabstop=4 :set smarttab :set shiftwidth=4 :set softtabstop=4 :set expandtab :set autoindent :set tw=79 ]],
+  pattern = {"py"}
+  }
+)
+
 
 vim.cmd("set mouse=ci")
 
@@ -15,3 +23,18 @@ vim.keymap.set('n', '<leader>y', '"*y')
 vim.keymap.set('n', '<leader>p', '"*p')
 vim.keymap.set('n', '<leader>Y', '"+y')
 vim.keymap.set('n', '<leader>P', '"+p')
+
+vim.keymap.set('n', '<C-Down>', 'ddp')
+vim.keymap.set('n', '<C-Up>', 'ddkP')
+vim.keymap.set('n', 'cc', 'diwi')
+
+function SET_DIRECTORY(name, option_name, path)
+  if (vim.fn.isdirectory(path)) then
+    vim.cmd("set "..name)
+    vim.cmd("set "..option_name.."="..path)
+  end
+end
+
+SET_DIRECTORY("backup","backupdir","~/.nvim/backup/")
+SET_DIRECTORY("undofile","undodir","~/.nvim/undo/")
+SET_DIRECTORY("swapfile","directory","~/.nvim/swap/")
