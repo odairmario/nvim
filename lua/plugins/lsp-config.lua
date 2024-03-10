5b3f1359511d57b377a5420d29a08813ab289f5b
Odair M. <odairmario45@gmail.com>
2024-03-09 22:17:37 -0300

    fix(Plugin): configure more lsp-config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "jedi_language_server", "pyre", "pyright", "pylsp", "ruff_lsp"}
+        ensure_installed = { "lua_ls", "tsserver", "jedi_language_server", "pyre", "pyright", "pylsp", "ruff_lsp", "ansiblels", "clangd", "marksman", "prosemd_lsp", "remark_ls", "vale_ls", "zk"}
       })
     end
   },
@@ -31,14 +31,61 @@ return {
         capabilities = capabilities
       })
       lspconfig.pyright.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
+        settings = {
+          python = {
+            analysis = {
+              autoImportCompletion = true,
+              autoSearchPaths = true,
+              diagnosticMode = 'openFilesOnly',
+              useLibraryCodeForTypes = true,
+              typeCheckingMode = 'standard'
+            }
+          }
+        }
       })
       lspconfig.pylsp.setup({
-        capabilities = capabilities
+        settings = {
+          pylsp = {
+            plugins = {
+                black = { enabled = true },
+                autopep8 = { enabled = true},
+                pylsp_mypy = { enabled = true },
+                jedi_completion = { fuzzy = true },
+                pyls_isort = { enabled = true },
+
+
+              }
+            }
+        },
+        capabilities = capabilities,
+
       })
       lspconfig.ruff_lsp.setup({
         capabilities = capabilities
       })
+      lspconfig.ansiblels.setup({
+        capabilities = capabilities
+      })
+      lspconfig.clangd.setup({
+        capabilities = capabilities
+      })
+      lspconfig.marksman.setup({
+        capabilities = capabilities
+      })
+      lspconfig.prosemd_lsp.setup({
+        capabilities = capabilities
+      })
+      lspconfig.remark_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.vale_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.zk.setup({
+        capabilities = capabilities
+      })
+
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition,{})
       vim.keymap.set('n', 'gr', vim.lsp.buf.references,{})
