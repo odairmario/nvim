ac5cfe8e0ca2b4105ad1bd22680a970a151c3e4d
Odair M. <odairmario45@gmail.com>
2024-03-09 22:15:24 -0300

    feat(Plugin): add solarized plugin

---
+++ lua/plugins/solarized.lua
@@ -0,0 +1,10 @@
+
+-- The colorscheme
+return {
+  "svrana/neosolarized.nvim",
+  comment_italics = true,
+  background_set = true,
+  --config = function()
+  --  vim.cmd.colorscheme  "neosolarized"
+  --end
+}
