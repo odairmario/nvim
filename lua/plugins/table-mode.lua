967b5279850005aeb06e4a8c8c990c439635516f
Odair M. <odairmario45@gmail.com>
2024-03-10 00:45:07 -0300

    fix(Plugin): enable tablemode for markdown and text file

--- lua/plugins/table-mode.lua
+++ lua/plugins/table-mode.lua
@@ -1,3 +1,10 @@
 return {
   'dhruvasagar/vim-table-mode',
+  config = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      command = [[:silent :TableModeEnable]],
+      pattern = { "md", "markdown", "text"}
+    }
+  )
+  end
 }
